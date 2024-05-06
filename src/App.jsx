e4b2dfd1bea8fd4b0921099195d2a639f3bdc445
stharavi01 <sanayashe@gmail.com>
2024-05-06 18:14:47 +0545

    add navbar and home animations

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,13 @@ import Home from "./components/Home";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import SideNavbar from "../src/components/SideNavbar";
+import { useEffect } from "react";
 
 const App = () => {
+	useEffect(() => {
+		// Scroll to the top of the page when the component mounts
+		window.scrollTo(0, 0);
+	}, []);
 	return (
 		<div>
 			<SideNavbar />
--- src/components/SideNavbar.jsx
+++ src/components/SideNavbar.jsx
@@ -1,11 +1,33 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { links, socials } from "../../data";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const SideNavbar = () => {
+	// Create an array of references for the list items
+	const navRefs = links.map(() => useRef(null));
 	const [isOpen, setIsOpen] = useState(false);
 
 	const toggleMenu = () => {
 		setIsOpen(!isOpen);
+	};
+
+	// Event handler for mouse enter
+	const handleMouseEnter = (ref) => {
+		gsap.to(ref.current, {
+			x: 25,
+			duration: 0.2,
+			ease: "power1.inOut",
+		});
+	};
+
+	// Event handler for mouse leave
+	const handleMouseLeave = (ref) => {
+		gsap.to(ref.current, {
+			x: 0,
+			duration: 0.5,
+			ease: "power1.inOut",
+		});
 	};
 
 	return (
@@ -17,7 +39,7 @@ const SideNavbar = () => {
 					className="focus:outline-none w-full h-full flex items-center justify-center"
 					onClick={toggleMenu}>
 					{/* Hamburger menu icon div */}
-					<div className="w-6 h-6 relative top-1">
+					<div class="w-6 h-6 relative top-1">
 						{/* Span elements for the hamburger and cross icons */}
 						<span
 							className={`block w-full h-0.5 bg-black transition-transform transform ${
@@ -37,29 +59,37 @@ const SideNavbar = () => {
 
 			{/* Side navigation menu */}
 			<div
-				className={`fixed top-0 left-0 h-full flex justify-center items-center w-64 md:w-96 text-2xl lg:text-4xl font-OpenSauce-Bold tracking-wide bg-lightBlack shadow-lg z-40  transition-transform duration-300 ease-in-out ${
+				className={`fixed top-0 left-0 h-full flex justify-center items-center w-64 md:w-96 text-2xl lg:text-4xl font-OpenSauce-Bold tracking-wide bg-lightBlack shadow-lg z-40 transition-transform duration-300 ease-in-out ${
 					isOpen ? "translate-x-0" : "-translate-x-full"
 				}`}>
 				{/* Navigation menu */}
-				<ul className="p-4 space-y-6">
-					<button className="focus:outline-none " onClick={toggleMenu}>
-						{links.map((link) => (
-							<li key={link.name}>
-								<a
-									href={link.hash}
-									className="block p-3 rounded text-lightWhite hover:text-white">
-									{link.name}
-								</a>
-							</li>
+				<ul>
+					{links.map((link, index) => (
+						<li
+							key={link.name}
+							ref={navRefs[index]}
+							className="p-4 space-y-6"
+							onMouseEnter={() => handleMouseEnter(navRefs[index])}
+							onMouseLeave={() => handleMouseLeave(navRefs[index])}>
+							<a
+								href={link.hash}
+								className="block p-3 rounded text-lightWhite hover:text-white">
+								{link.name}
+							</a>
+						</li>
+					))}
+					<div className="flex mt-12 gap-6 ">
+						{socials.map((social) => (
+							<a
+								key={social.name}
+								href={social.link}
+								target="_blank"
+								rel="noopener noreferrer"
+								className="transform hover:scale-125 transition">
+								<span className="">{social.icon}</span>
+							</a>
 						))}
-						<div className="flex mt-12 gap-6">
-							{socials.map((social) => (
-								<a key={social.name} href={social.link} target="_blank">
-									<span>{social.icon}</span>
-								</a>
-							))}
-						</div>
-					</button>
+					</div>
 				</ul>
 			</div>
 		</div>
